9751562ef5ec650b10d56fc1c595112a4299f666
deeeki <deeeki@gmail.com>
2014-05-04 21:57:34 +0900

    Configure generators

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,11 @@ module RakutenCampaign
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.generators do |g|
+      g.helper false
+      g.javascripts false
+      g.stylesheets false
+    end
   end
 end
