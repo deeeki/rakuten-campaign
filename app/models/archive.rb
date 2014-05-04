149e19849730e20da7704ca58b2cadcdb33e3edf
deeeki <deeeki@gmail.com>
2014-05-05 00:33:46 +0900

    Define relations and validations

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -1,2 +1,5 @@
 class Archive < ActiveRecord::Base
+  has_many :campaigns, dependent: :delete_all
+
+  validate :displayed_on, presence: true
 end
--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,5 @@
 class Campaign < ActiveRecord::Base
   belongs_to :archive
+
+  validate :url, presence: true
 end
