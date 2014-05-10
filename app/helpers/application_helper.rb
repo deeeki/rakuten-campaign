aeccd1c8e688cb1e6ccd3661e99384dc766fff25
deeeki <deeeki@gmail.com>
2014-05-10 20:29:13 +0900

    Emphasize point rates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def emphasize_point_rates str
+    rates = str.to_s.scan(/ポイント.*?\d倍/).uniq
+    return str if rates.empty?
+    raw(rates.inject(str){|str, rate| str.gsub(rate, content_tag(:strong, rate)) })
+  end
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'noreply@rakuten-campaign.herokuapp.com', to: ENV['EMAIL_TO']
+  helper :application
 
   def campaigns
     @archive = Archive.last
