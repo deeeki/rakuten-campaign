1cc07988c7aa0640fb68459ed1f217390dea979f
deeeki <deeeki@gmail.com>
2015-08-08 17:51:48 +0900

    Use `includes` to prevent N+1

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -1,5 +1,5 @@
 class ArchivesController < ApplicationController
   def index
-    @archives = Archive.order(displayed_on: :desc).limit(7)
+    @archives = Archive.includes(:campaigns).order(displayed_on: :desc).limit(7)
   end
 end
